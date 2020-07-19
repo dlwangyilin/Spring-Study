5653b166ec3a8ba5375d69b09c153ac42858ca4d
dlwangyilin <dlwangyilin@gmail.com>
2020-07-19 14:26:04 -0700

    index page for convenience

--- src/main/java/com/yilin/springdemo/rest/DemoRestController.java
+++ src/main/java/com/yilin/springdemo/rest/DemoRestController.java
@@ -12,7 +12,7 @@ public class DemoRestController {
 	
 	@GetMapping("/hello")
 	public String sayHello() {
-		return "Hello World!";
+		return "Hello from REST CONTROLLER!";
 	}
 	
 }
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<a href=”${pageContext.request.contextPath}/test/hello">GOTO TEST LINK</a>
+</body>
+</html>
