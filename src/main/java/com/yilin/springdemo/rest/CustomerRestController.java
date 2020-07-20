e9eaf22e2f8e6f0ec38c1a99763a7ec581c5b7cf
dlwangyilin <dlwangyilin@gmail.com>
2020-07-19 20:46:31 -0700

    Add exception handler.

--- src/main/java/com/yilin/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/yilin/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import com.yilin.springdemo.entity.Customer;
 import com.yilin.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +18,20 @@ public class CustomerRestController {
     @Autowired
     private CustomerService customerService;
 
-    // add mappings
+    // add mappings for GET /customers
     @GetMapping("/customers")
     public List<Customer> getCustomers() {
         return customerService.getCustomers();
+    }
+
+    // add mappings for GET /customers/:customerId
+    @GetMapping("/customers/{customerId}")
+    public Customer getCustomer(@PathVariable int customerId) {
+
+        Customer customer = customerService.getCustomer(customerId);
+        if (customer == null) {
+            throw new CustomerNotFoundException("Customer is not found");
+        }
+        return customer;
     }
 }
