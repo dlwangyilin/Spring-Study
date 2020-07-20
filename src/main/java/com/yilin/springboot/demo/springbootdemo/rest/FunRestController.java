1ce8c0b7bbfc7a6948868aa7e6a50f530d3e8700
dlwangyilin <dlwangyilin@gmail.com>
2020-07-20 16:55:27 -0700

    More configuration.

--- src/main/java/com/yilin/springboot/demo/springbootdemo/rest/FunRestController.java
+++ src/main/java/com/yilin/springboot/demo/springbootdemo/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.yilin.springboot.demo.springbootdemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,28 @@ import java.time.LocalDateTime;
 @RestController
 public class FunRestController {
 
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
     // expose /
     @GetMapping("/")
     public String sayHello() {
         return "Hello word on " + LocalDateTime.now();
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Good Luck";
     }
 }
