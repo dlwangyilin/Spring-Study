1c0da29cbee690ed3c4f49505d436373a5f76cf1
dlwangyilin <dlwangyilin@gmail.com>
2020-07-19 22:18:39 -0700

    spring boot project provided by spring-initializer

---
+++ src/main/java/com/yilin/springboot/demo/springbootdemo/SpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yilin.springboot.demo.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoApplication.class, args);
+	}
+
+}
