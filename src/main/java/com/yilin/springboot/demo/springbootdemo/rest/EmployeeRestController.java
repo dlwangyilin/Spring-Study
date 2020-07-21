aea3afb957bd28cd46d11733deef41c89c5064cc
dlwangyilin <dlwangyilin@gmail.com>
2020-07-20 22:23:58 -0700

    All CRUD

--- src/main/java/com/yilin/springboot/demo/springbootdemo/rest/EmployeeRestController.java
+++ src/main/java/com/yilin/springboot/demo/springbootdemo/rest/EmployeeRestController.java
@@ -4,9 +4,7 @@ import com.yilin.springboot.demo.springbootdemo.dao.EmployeeDAO;
 import com.yilin.springboot.demo.springbootdemo.entity.Employee;
 import com.yilin.springboot.demo.springbootdemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +25,39 @@ public class EmployeeRestController {
         return employeeService.findAll();
     }
 
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        // this is to force a save of new item
+        employee.setId(0);
+
+        employeeService.save(employee);
+
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.save(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        employeeService.deleteById(employeeId);
+        return "Deleted employee id - " + employeeId;
+    }
 
 }
