bb822020ebe646575b9292f2c2efd3d0062c25f0
dlwangyilin <dlwangyilin@gmail.com>
2020-07-20 23:31:26 -0700

    Configuration. @RepositoryRestResource(path = "members") - endpoint spring.data.rest.base-path=/magic-api - localhost:8080/magic-api/members spring.data.rest.default-page-size=20 - pagesize

--- src/main/java/com/yilin/springboot/demo/springbootdemo/dao/EmployeeRepository.java
+++ src/main/java/com/yilin/springboot/demo/springbootdemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.yilin.springboot.demo.springbootdemo.dao;
 
 import com.yilin.springboot.demo.springbootdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
